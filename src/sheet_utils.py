fa09031409e1430724f1b5a070e95a68d06a90c4
Erwin Lambert <e.lambert@uu.nl>
2020-12-14 21:31:20 +0100

    Update sheet_utils.py

--- src/sheet_utils.py
+++ src/sheet_utils.py
@@ -3,8 +3,24 @@ import xarray as xr
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import cmocean as cmo
+from numba import jit
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+@jit(nopython=True)
+def roll_ym1(X):
+    return np.reshape(np.roll(np.ravel(X),-X.shape[1]),X.shape)
+
+@jit(nopython=True)
+def roll_yp1(X):
+    return np.reshape(np.roll(np.ravel(X),X.shape[1]),X.shape)
+
+@jit(nopython=True)
+def roll_xm1(X):
+    return np.transpose(np.reshape(np.roll(np.ravel(np.transpose(X)),-X.shape[0]),(X.shape[1],X.shape[0])))
+
+@jit(nopython=True)
+def roll_xp1(X):
+    return np.transpose(np.reshape(np.roll(np.ravel(np.transpose(X)),X.shape[0]),(X.shape[1],X.shape[0])))
 
 def create_mask(self):
         
@@ -15,23 +31,23 @@ def create_mask(self):
     self.ocn   = np.where(self.mask==0,1,0)
 
     #Rolled masks
-    self.tmaskym1    = np.roll(self.tmask,-1,axis=0)
-    self.tmaskyp1    = np.roll(self.tmask, 1,axis=0)
-    self.tmaskxm1    = np.roll(self.tmask,-1,axis=1)
-    self.tmaskxp1    = np.roll(self.tmask, 1,axis=1)    
-    self.tmaskxm1ym1 = np.roll(np.roll(self.tmask,-1,axis=0),-1,axis=1)
-    self.tmaskxm1yp1 = np.roll(np.roll(self.tmask, 1,axis=0),-1,axis=1)
-    self.tmaskxp1ym1 = np.roll(np.roll(self.tmask,-1,axis=0), 1,axis=1)
+    self.tmaskym1    = roll_ym1(self.tmask)
+    self.tmaskyp1    = roll_yp1(self.tmask)
+    self.tmaskxm1    = roll_xm1(self.tmask)
+    self.tmaskxp1    = roll_xp1(self.tmask)
+    self.tmaskxm1ym1 = roll_xm1(roll_ym1(self.tmask))
+    self.tmaskxm1yp1 = roll_xm1(roll_yp1(self.tmask))
+    self.tmaskxp1ym1 = roll_xp1(roll_ym1(self.tmask))
 
-    self.ocnym1      = np.roll(self.ocn,-1,axis=0)
-    self.ocnyp1      = np.roll(self.ocn, 1,axis=0)
-    self.ocnxm1      = np.roll(self.ocn,-1,axis=1)
-    self.ocnxp1      = np.roll(self.ocn, 1,axis=1)
+    self.ocnym1      = roll_ym1(self.ocn)
+    self.ocnyp1      = roll_yp1(self.ocn)
+    self.ocnxm1      = roll_xm1(self.ocn)
+    self.ocnxp1      = roll_xp1(self.ocn)
     
-    self.grdNu = 1-np.roll((1-self.grd)*(1-np.roll(self.grd,-1,axis=1)),-1,axis=0)
-    self.grdSu = 1-np.roll((1-self.grd)*(1-np.roll(self.grd,-1,axis=1)), 1,axis=0)
-    self.grdEv = 1-np.roll((1-self.grd)*(1-np.roll(self.grd,-1,axis=0)),-1,axis=1)
-    self.grdWv = 1-np.roll((1-self.grd)*(1-np.roll(self.grd,-1,axis=0)), 1,axis=1)
+    self.grdNu = 1-roll_ym1((1-self.grd)*(1-roll_xm1(self.grd)))
+    self.grdSu = 1-roll_yp1((1-self.grd)*(1-roll_xm1(self.grd)))
+    self.grdEv = 1-roll_xm1((1-self.grd)*(1-roll_ym1(self.grd)))
+    self.grdWv = 1-roll_xp1((1-self.grd)*(1-roll_ym1(self.grd)))
     
     #Extract ice shelf front
     self.isfE = self.ocn*self.tmaskxp1
@@ -48,11 +64,11 @@ def create_mask(self):
     self.grl  = self.grlE+self.grlN+self.grlW+self.grlS
     
     #Create masks for U- and V- velocities at N/E faces of grid points
-    self.umask = (self.tmask+self.isfW)*(1-np.roll(self.grlE,-1,axis=1))
-    self.vmask = (self.tmask+self.isfS)*(1-np.roll(self.grlN,-1,axis=0))
+    self.umask = (self.tmask+self.isfW)*(1-roll_xm1(self.grlE))
+    self.vmask = (self.tmask+self.isfS)*(1-roll_ym1(self.grlN))
     
-    self.umaskxp1 = np.roll(self.umask,1,axis=1)
-    self.vmaskyp1 = np.roll(self.vmask,1,axis=0)
+    self.umaskxp1 = roll_xp1(self.umask)
+    self.vmaskyp1 = roll_yp1(self.vmask)
 
     return
 
@@ -106,105 +122,98 @@ def initialize_vars(self):
     intS(self,self.dt)
     return
 
+@jit(nopython=True)
 def div0(a,b):
-    return np.divide(a,b,out=np.zeros_like(a), where=b!=0)
+    return np.where(b>0,a/b,0)
 
+@jit(nopython=True)
 def im(var):
     """Value at i-1/2 """
-    return .5*(var+np.roll(var,1,axis=1))
+    return .5*(var+roll_xp1(var))
     
+@jit(nopython=True)
 def ip(var):
     """Value at i+1/2"""
-    return .5*(var+np.roll(var,-1,axis=1))
-    
+    return .5*(var+roll_xm1(var))
+
+@jit(nopython=True)    
 def jm(var):
     """Value at j-1/2"""
-    return .5*(var+np.roll(var,1,axis=0))
-    
+    return .5*(var+roll_yp1(var))
+
+@jit(nopython=True)    
 def jp(var):
     """Value at j+1/2"""
-    return .5*(var+np.roll(var,-1,axis=0))
+    return .5*(var+roll_ym1(var))
 
-def im_t(self,var):
-    """Value at i-1/2, no gradient across boundary"""
-    return div0((var*self.tmask + np.roll(var*self.tmask, 1,axis=1)),self.tmask+self.tmaskxp1)
-
-def ip_t(self,var):
-    """Value at i+1/2, no gradient across boundary"""   
-    return div0((var*self.tmask + np.roll(var*self.tmask,-1,axis=1)),self.tmask+self.tmaskxm1)
-
-def jm_t(self,var):
-    """Value at j-1/2, no gradient across boundary"""
-    return div0((var*self.tmask + np.roll(var*self.tmask, 1,axis=0)),self.tmask+self.tmaskyp1)
-
-def jp_t(self,var):
-    """Value at j+1/2, no gradient across boundary"""
-    return div0((var*self.tmask + np.roll(var*self.tmask,-1,axis=0)),self.tmask+self.tmaskym1)
-
+@jit(nopython=True)
 def im_(var,mask):
     """Value at i-1/2, no gradient across boundary"""
-    return div0((var*mask + np.roll(var*mask, 1,axis=1)),(mask+np.roll(mask, 1,axis=1)))
+    return div0((var*mask + roll_xp1(var*mask)),(mask+roll_xp1(mask)))
 
+@jit(nopython=True)
 def ip_(var,mask):
     """Value at i+1/2, no gradient across boundary"""
-    return div0((var*mask + np.roll(var*mask,-1,axis=1)),(mask+np.roll(mask,-1,axis=1)))
+    return div0((var*mask + roll_xm1(var*mask)),(mask+roll_xm1(mask)))
 
+@jit(nopython=True)
 def jm_(var,mask):
     """Value at j-1/2, no gradient across boundary"""
-    return div0((var*mask + np.roll(var*mask, 1,axis=0)),(mask+np.roll(mask, 1,axis=0)))
+    return div0((var*mask + roll_yp1(var*mask)),(mask+roll_yp1(mask)))
 
+@jit(nopython=True)
 def jp_(var,mask):
     """Value at j+1/2, no gradient across boundary"""
-    return div0((var*mask + np.roll(var*mask,-1,axis=0)),(mask+np.roll(mask,-1,axis=0)))
+    return div0((var*mask + roll_ym1(var*mask)),(mask+roll_ym1(mask)))
 
 def lapT(self,var):
     """Laplacian operator for DT and DS"""
     
-    tN = jp_t(self,self.D[0,:,:])*(np.roll(var,-1,axis=0)-var)*self.tmaskym1/self.dy**2
-    tS = jm_t(self,self.D[0,:,:])*(np.roll(var, 1,axis=0)-var)*self.tmaskyp1/self.dy**2
-    tE = ip_t(self,self.D[0,:,:])*(np.roll(var,-1,axis=1)-var)*self.tmaskxm1/self.dx**2
-    tW = im_t(self,self.D[0,:,:])*(np.roll(var, 1,axis=1)-var)*self.tmaskxp1/self.dx**2    
+    tN = jp_(self.D[0,:,:],self.tmask)*(roll_ym1(var)-var)*self.tmaskym1/self.dy**2
+    tS = jm_(self.D[0,:,:],self.tmask)*(roll_yp1(var)-var)*self.tmaskyp1/self.dy**2
+    tE = ip_(self.D[0,:,:],self.tmask)*(roll_xm1(var)-var)*self.tmaskxm1/self.dx**2
+    tW = im_(self.D[0,:,:],self.tmask)*(roll_xp1(var)-var)*self.tmaskxp1/self.dx**2    
     
     return tN+tS+tE+tW
 
 def lapu(self):
     """Laplacian operator for Du"""
-    Dcent = ip_t(self,self.D[0,:,:])
+    Dcent = ip_(self.D[0,:,:],self.tmask)
     var = self.u[0,:,:]
 
-    tN = jp_t(self,Dcent) * (np.roll(var,-1,axis=0)-var)/self.dy**2 * (1-self.ocnym1) - self.slip*Dcent*var*self.grdNu/self.dy**2
-    tS = jm_t(self,Dcent) * (np.roll(var, 1,axis=0)-var)/self.dy**2 * (1-self.ocnyp1) - self.slip*Dcent*var*self.grdSu/self.dy**2  
-    tE = self.Dxm1        * (np.roll(var,-1,axis=1)-var)/self.dx**2 * (1-self.ocnxm1)
-    tW = self.D[0,:,:]    * (np.roll(var, 1,axis=1)-var)/self.dx**2 * (1-self.ocn   )
+    tN = jp_(Dcent,self.tmask) * (roll_ym1(var)-var)/self.dy**2 * (1-self.ocnym1) - self.slip*Dcent*var*self.grdNu/self.dy**2
+    tS = jm_(Dcent,self.tmask) * (roll_yp1(var)-var)/self.dy**2 * (1-self.ocnyp1) - self.slip*Dcent*var*self.grdSu/self.dy**2  
+    tE = self.Dxm1             * (roll_xm1(var)-var)/self.dx**2 * (1-self.ocnxm1)
+    tW = self.D[0,:,:]         * (roll_xp1(var)-var)/self.dx**2 * (1-self.ocn   )
     
     return (tN+tS+tE+tW) * self.umask
 
 def lapv(self):
     """Laplacian operator for Dv"""
-    Dcent = jp_t(self,self.D[0,:,:])
+    Dcent = jp_(self.D[0,:,:],self.tmask)
     var = self.v[0,:,:]
     
-    tN = self.Dym1        * (np.roll(var,-1,axis=0)-var)/self.dy**2 * (1-self.ocnym1) 
-    tS = self.D[0,:,:]    * (np.roll(var, 1,axis=0)-var)/self.dy**2 * (1-self.ocn   )
-    tE = ip_t(self,Dcent) * (np.roll(var,-1,axis=1)-var)/self.dx**2 * (1-self.ocnxm1) - self.slip*Dcent*var*self.grdEv/self.dx**2
-    tW = im_t(self,Dcent) * (np.roll(var, 1,axis=1)-var)/self.dx**2 * (1-self.ocnxp1) - self.slip*Dcent*var*self.grdWv/self.dx**2  
+    tN = self.Dym1             * (roll_ym1(var)-var)/self.dy**2 * (1-self.ocnym1) 
+    tS = self.D[0,:,:]         * (roll_yp1(var)-var)/self.dy**2 * (1-self.ocn   )
+    tE = ip_(Dcent,self.tmask) * (roll_xm1(var)-var)/self.dx**2 * (1-self.ocnxm1) - self.slip*Dcent*var*self.grdEv/self.dx**2
+    tW = im_(Dcent,self.tmask) * (roll_xp1(var)-var)/self.dx**2 * (1-self.ocnxp1) - self.slip*Dcent*var*self.grdWv/self.dx**2  
     
     return (tN+tS+tE+tW) * self.vmask
 
 def convT(self,var):
     """Upstream convergence scheme for D, DT, DS"""
-    tN = - (np.maximum(self.v[1,:,:],0)*var                   + np.minimum(self.v[1,:,:],0)*np.roll(var,-1,axis=0)) / self.dy * self.vmask
-    tS =   (np.maximum(self.vyp1    ,0)*np.roll(var,1,axis=0) + np.minimum(self.vyp1    ,0)*var                   ) / self.dy * self.vmaskyp1
-    tE = - (np.maximum(self.u[1,:,:],0)*var                   + np.minimum(self.u[1,:,:],0)*np.roll(var,-1,axis=1)) / self.dx * self.umask
-    tW =   (np.maximum(self.uxp1    ,0)*np.roll(var,1,axis=1) + np.minimum(self.uxp1    ,0)*var                   ) / self.dx * self.umaskxp1
+    tN = - (np.maximum(self.v[1,:,:],0)*var           + np.minimum(self.v[1,:,:],0)*roll_ym1(var)) / self.dy * self.vmask
+    tS =   (np.maximum(self.vyp1    ,0)*roll_yp1(var) + np.minimum(self.vyp1    ,0)*var          ) / self.dy * self.vmaskyp1
+    tE = - (np.maximum(self.u[1,:,:],0)*var           + np.minimum(self.u[1,:,:],0)*roll_xm1(var)) / self.dx * self.umask
+    tW =   (np.maximum(self.uxp1    ,0)*roll_xp1(var) + np.minimum(self.uxp1    ,0)*var          ) / self.dx * self.umaskxp1
     return (tN+tS+tE+tW) * self.tmask
 
 def convTcen(self,var):
     """Centered convergence scheme for D, T, and S"""
-    tN = -jp_t(self,var)*self.v[1,:,:] /self.dy * self.vmask                             
-    tS =  jm_t(self,var)*self.vyp1     /self.dy * self.vmaskyp1
-    tE = -ip_t(self,var)*self.u[1,:,:] /self.dx * self.umask                             
-    tW =  im_t(self,var)*self.uxp1     /self.dx * self.umaskxp1
+    tN = -jp_(var,self.tmask)*self.v[1,:,:] /self.dy * self.vmask                             
+    tS =  jm_(var,self.tmask)*self.vyp1     /self.dy * self.vmaskyp1
+    tE = -ip_(var,self.tmask)*self.u[1,:,:] /self.dx * self.umask                             
+    tW =  im_(var,self.tmask)*self.uxp1     /self.dx * self.umaskxp1
     return (tN+tS+tE+tW) * self.tmask
 
 def convu(self):
@@ -216,10 +225,10 @@ def convu(self):
     DE = self.Dxm1                 + self.ocnxm1 * self.D[1,:,:]*self.tmask
     DW = self.D[1,:,:]*self.tmask  + self.ocn    * self.Dxm1
     
-    tN = -DN *        ip_(self.v[1,:,:],self.vmask)           *(jp_(self.u[1,:,:],self.umask)-self.slip*self.u[1,:,:]*self.grdNu) /self.dy
-    tS =  DS *np.roll(ip_(self.v[1,:,:],self.vmask),1,axis=0) *(jm_(self.u[1,:,:],self.umask)-self.slip*self.u[1,:,:]*self.grdSu) /self.dy
-    tE = -DE *        ip_(self.u[1,:,:],self.umask)           * ip_(self.u[1,:,:],self.umask)                                     /self.dx
-    tW =  DW *        im_(self.u[1,:,:],self.umask)           * im_(self.u[1,:,:],self.umask)                                     /self.dx
+    tN = -DN *         ip_(self.v[1,:,:],self.vmask)  *(jp_(self.u[1,:,:],self.umask)-self.slip*self.u[1,:,:]*self.grdNu) /self.dy
+    tS =  DS *roll_yp1(ip_(self.v[1,:,:],self.vmask)) *(jm_(self.u[1,:,:],self.umask)-self.slip*self.u[1,:,:]*self.grdSu) /self.dy
+    tE = -DE *         ip_(self.u[1,:,:],self.umask)  * ip_(self.u[1,:,:],self.umask)                                     /self.dx
+    tW =  DW *         im_(self.u[1,:,:],self.umask)  * im_(self.u[1,:,:],self.umask)                                     /self.dx
     
     return (tN+tS+tE+tW) * self.umask
 
@@ -232,10 +241,10 @@ def convv(self):
     DN = self.Dym1                 + self.ocnym1 * self.D[1,:,:]*self.tmask
     DS = self.D[1,:,:]*self.tmask  + self.ocn    * self.Dym1 
     
-    tN = -DN *        jp_(self.v[1,:,:],self.vmask)           * jp_(self.v[1,:,:],self.vmask)                                     /self.dy
-    tS =  DS *        jm_(self.v[1,:,:],self.vmask)           * jm_(self.v[1,:,:],self.vmask)                                     /self.dy
-    tE = -DE *        jp_(self.u[1,:,:],self.umask)           *(ip_(self.v[1,:,:],self.vmask)-self.slip*self.v[1,:,:]*self.grdEv) /self.dx
-    tW =  DW *np.roll(jp_(self.u[1,:,:],self.umask),1,axis=1) *(im_(self.v[1,:,:],self.vmask)-self.slip*self.v[1,:,:]*self.grdWv) /self.dx
+    tN = -DN *         jp_(self.v[1,:,:],self.vmask)  * jp_(self.v[1,:,:],self.vmask)                                     /self.dy
+    tS =  DS *         jm_(self.v[1,:,:],self.vmask)  * jm_(self.v[1,:,:],self.vmask)                                     /self.dy
+    tE = -DE *         jp_(self.u[1,:,:],self.umask)  *(ip_(self.v[1,:,:],self.vmask)-self.slip*self.v[1,:,:]*self.grdEv) /self.dx
+    tW =  DW *roll_xp1(jp_(self.u[1,:,:],self.umask)) *(im_(self.v[1,:,:],self.vmask)-self.slip*self.v[1,:,:]*self.grdWv) /self.dx
     
     return (tN+tS+tE+tW) * self.vmask     
 
@@ -250,16 +259,16 @@ def updatesecondary(self):
     self.entr = self.E0*(np.abs(im(self.u[1,:,:])*self.dzdx + jm(self.v[1,:,:])*self.dzdy)) * self.tmask
     self.melt = self.cp/self.L*self.CG*(im(self.u[1,:,:])**2+jm(self.v[1,:,:])**2)**.5*(self.T[1,:,:]-self.Tf) * self.tmask
     
-    self.Dym1    = np.roll(        self.D[1,:,:]*self.tmask,-1,axis=0)
-    self.Dyp1    = np.roll(        self.D[1,:,:]*self.tmask, 1,axis=0)
-    self.Dxm1    = np.roll(        self.D[1,:,:]*self.tmask,-1,axis=1)
-    self.Dxp1    = np.roll(        self.D[1,:,:]*self.tmask, 1,axis=1)
-    self.Dxm1ym1 = np.roll(np.roll(self.D[1,:,:]*self.tmask,-1,axis=1),-1,axis=0)
-    self.Dxp1ym1 = np.roll(np.roll(self.D[1,:,:]*self.tmask, 1,axis=1),-1,axis=0)
-    self.Dxm1yp1 = np.roll(np.roll(self.D[1,:,:]*self.tmask,-1,axis=1), 1,axis=0)
+    self.Dym1    = roll_ym1(         self.D[1,:,:]*self.tmask)
+    self.Dyp1    = roll_yp1(         self.D[1,:,:]*self.tmask)
+    self.Dxm1    = roll_xm1(         self.D[1,:,:]*self.tmask)
+    self.Dxp1    = roll_xp1(         self.D[1,:,:]*self.tmask)
+    self.Dxm1ym1 = roll_ym1(roll_xm1(self.D[1,:,:]*self.tmask))
+    self.Dxp1ym1 = roll_ym1(roll_xp1(self.D[1,:,:]*self.tmask))
+    self.Dxm1yp1 = roll_yp1(roll_xm1(self.D[1,:,:]*self.tmask))
     
-    self.vyp1    = np.roll(self.v[1,:,:],1,axis=0)  
-    self.uxp1    = np.roll(self.u[1,:,:],1,axis=1)      
+    self.vyp1    = roll_yp1(self.v[1,:,:])  
+    self.uxp1    = roll_xp1(self.u[1,:,:])      
 
 def intD(self,delt):
     """Integrate D"""
@@ -273,28 +282,28 @@ def intD(self,delt):
 def intu(self,delt):
     """Integrate u"""
     self.u[2,:,:] = self.u[0,:,:] \
-                    +div0((-self.u[1,:,:] * ip_t(self,(self.D[2,:,:]-self.D[0,:,:]))/(2*self.dt) \
+                    +div0((-self.u[1,:,:] * ip_(self.D[2,:,:]-self.D[0,:,:],self.tmask)/(2*self.dt) \
                     +  convu(self) \
-                    +  -self.g*ip_t(self,self.drho*self.D[1,:,:])*(self.Dxm1-self.D[1,:,:])/self.dx * self.tmask*self.tmaskxm1 \
-                    +  self.g*ip_t(self,self.drho*self.D[1,:,:]*self.dzdx) \
-                    +  -.5*self.g*ip_t(self,self.D[1,:,:])**2*(np.roll(self.drho,-1,axis=1)-self.drho)/self.dx * self.tmask * self.tmaskxm1 \
-                    +  self.f*ip_t(self,self.D[1,:,:]*jm_(self.v[1,:,:],self.vmask)) \
+                    +  -self.g*ip_(self.drho*self.D[1,:,:],self.tmask)*(self.Dxm1-self.D[1,:,:])/self.dx * self.tmask*self.tmaskxm1 \
+                    +  self.g*ip_(self.drho*self.D[1,:,:]*self.dzdx,self.tmask) \
+                    +  -.5*self.g*ip_(self.D[1,:,:],self.tmask)**2*(roll_xm1(self.drho)-self.drho)/self.dx * self.tmask * self.tmaskxm1 \
+                    +  self.f*ip_(self.D[1,:,:]*jm_(self.v[1,:,:],self.vmask),self.tmask) \
                     +  -self.Cd*self.u[1,:,:]*(self.u[1,:,:]**2 + ip(jm(self.v[1,:,:]))**2)**.5 \
                     +  self.Ah*lapu(self)
-                    ),ip_t(self,self.D[1,:,:])) * self.umask * delt
+                    ),ip_(self.D[1,:,:],self.tmask)) * self.umask * delt
 
 def intv(self,delt):
     """Integrate v"""
     self.v[2,:,:] = self.v[0,:,:] \
-                    +div0((-self.v[1,:,:] * jp_t(self,(self.D[2,:,:]-self.D[0,:,:]))/(2*self.dt) \
+                    +div0((-self.v[1,:,:] * jp_(self.D[2,:,:]-self.D[0,:,:],self.tmask)/(2*self.dt) \
                     + convv(self) \
-                    + -self.g*jp_t(self,self.drho*self.D[1,:,:])*(self.Dym1-self.D[1,:,:])/self.dy * self.tmask*self.tmaskym1 \
-                    + self.g*jp_t(self,self.drho*self.D[1,:,:]*self.dzdy) \
-                    + -.5*self.g*jp_t(self,self.D[1,:,:])**2*(np.roll(self.drho,-1,axis=0)-self.drho)/self.dy * self.tmask * self.tmaskym1 \
-                    + -self.f*jp_t(self,self.D[1,:,:]*im_(self.u[1,:,:],self.umask)) \
+                    + -self.g*jp_(self.drho*self.D[1,:,:],self.tmask)*(self.Dym1-self.D[1,:,:])/self.dy * self.tmask*self.tmaskym1 \
+                    + self.g*jp_(self.drho*self.D[1,:,:]*self.dzdy,self.tmask) \
+                    + -.5*self.g*jp_(self.D[1,:,:],self.tmask)**2*(roll_ym1(self.drho)-self.drho)/self.dy * self.tmask * self.tmaskym1 \
+                    + -self.f*jp_(self.D[1,:,:]*im_(self.u[1,:,:],self.umask),self.tmask) \
                     + -self.Cd*self.v[1,:,:]*(self.v[1,:,:]**2 + jp(im(self.u[1,:,:]))**2)**.5 \
                     + self.Ah*lapv(self)
-                    ),jp_t(self,self.D[1,:,:])) * self.vmask * delt
+                    ),jp_(self.D[1,:,:],self.tmask)) * self.vmask * delt
     
 def intT(self,delt):
     """Integrate T"""
